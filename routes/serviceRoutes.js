90552b6e6991711bb22c9af5f3d59b05dbdddcf9
mohammedateeef <atefm.me556@gmail.com>
2024-09-18 09:36:53 +0300

    changes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { verifyOTPCode, sendOTPCode, followUpService} = require('../controllers/serviceController');
+const { verifyOTPCode, sendOTPCode, followUpServiceData} = require('../controllers/serviceController');
 const { createAndUpdateService } = require('../utils/validators/serviceValidator')
 
 const router = express.Router();
 
 router.post('/send-otp', sendOTPCode);
 router.post('/verify-otp', verifyOTPCode);
-router.post('/follow-up/:id', createAndUpdateService, followUpService);
+router.post('/follow-up/:id', createAndUpdateService, followUpServiceData);
 
 module.exports = router;
